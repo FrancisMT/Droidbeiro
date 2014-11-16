d0466ae8a9107de20b7764ec34e99758d0090c34
android4setec <android4>
2014-11-16 15:59:28 +0000

    Inicio da Introdução da interface

---
+++ app/src/main/java/pt/up/fe/droidbeiro/Communication/ServerThread.java
@@ -0,0 +1,7 @@
+package pt.up.fe.droidbeiro.Communication;
+
+/**
+ * Created by Francisco on 16/11/2014.
+ */
+public class ServerThread {
+}
---
+++ app/src/main/java/pt/up/fe/droidbeiro/Service/BLE.java
@@ -0,0 +1,7 @@
+package pt.up.fe.droidbeiro.Service;
+
+/**
+ * Created by Francisco on 16/11/2014.
+ */
+public class BLE {
+}
