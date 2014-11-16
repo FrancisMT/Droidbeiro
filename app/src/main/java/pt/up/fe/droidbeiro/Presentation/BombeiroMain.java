3ea580bb9ff862909891be541e001b2bc4700d3b
android4setec <android4>
2014-11-16 22:42:37 +0000

    Continuação da Introdução da interface

--- app/src/main/java/pt/up/fe/droidbeiro/Presentation/BombeiroMain.java
+++ app/src/main/java/pt/up/fe/droidbeiro/Presentation/BombeiroMain.java
@@ -2,11 +2,19 @@ package pt.up.fe.droidbeiro.Presentation;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
 
 
 import pt.up.fe.droidbeiro.R;
@@ -14,12 +22,23 @@ import pt.up.fe.droidbeiro.R;
 public class BombeiroMain extends Activity {
 
     // Initialize the array
-    String[] monthsArray = { "JAN", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY", "AUG", "SEPT", "OCT", "NOV", "DEC" };
+    String[] messages = {   "Preciso de ajuda",
+                            "Preciso afastar-me",
+                            "Preciso de suporte aéreo",
+                            "Fogo a espalhar-se",
+                            "Fogo perto de casa",
+                            "Camião com problemas",
+                            "Casa queimada",
+                            "A retirar-me",
+                        };
 
     // Declare the UI components
-    private ListView monthsListView;
-
+    private ListView lista_mensagens_layout;
     private ArrayAdapter arrayAdapter;
+    private Button btn_enviar_msg;
+    private String mensagem;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +48,47 @@ public class BombeiroMain extends Activity {
         // Hiding the action bar
         getActionBar().hide();
 
-        monthsListView = (ListView) findViewById(R.id.lista_mensagens);
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, monthsArray);
-        monthsListView.setAdapter(arrayAdapter);
+        lista_mensagens_layout = (ListView) findViewById(R.id.lista_mensagens);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, messages);
+        lista_mensagens_layout.setAdapter(arrayAdapter);
 
+        btn_enviar_msg = (Button)findViewById(R.id.btn_enviar_msg);
+
+        lista_mensagens_layout.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                mensagem=lista_mensagens_layout.getItemAtPosition(position).toString();
+
+
+
+            }
+        });
+
+        btn_enviar_msg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               AlertDialog.Builder alertDialog = new AlertDialog.Builder(BombeiroMain.this);
+               alertDialog.setTitle("Enviar mensagem ?");
+               alertDialog.setMessage(mensagem);
+
+                //Setting Positive "Sim" Button
+                    alertDialog.setPositiveButton("Enviar", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog,int which) {
+                        //envia a mensagem para o centro de controlo
+                    }
+                });
+
+                // Setting Negative "NÃO" Button
+                alertDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //envia a mensagem para o centro de controlo
+                    }
+                });
+
+                alertDialog.show();
+
+            }
+        });
 
 
 
